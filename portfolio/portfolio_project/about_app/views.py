39fa8d31f2fdd95d00dd00c533d221ba013d408d
arielnasimba <arielkafe@hotmail.com>
2024-05-02 14:11:51 +0200

    add templates folder

--- portfolio/portfolio_project/about_app/views.py
+++ portfolio/portfolio_project/about_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_about(request):
+    
+    return render(request, 'frontend/about.html')
--- portfolio/portfolio_project/portfolio_project/urls.py
+++ portfolio/portfolio_project/portfolio_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from about_app import views as aboutViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', aboutViews.home_about , name="about_section"),
 ]
